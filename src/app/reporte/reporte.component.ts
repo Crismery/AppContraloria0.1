567dce2144daf791a365bea57720a650bc453c45
Crismery <crismerydisla0550@gmail.com>
2024-04-03 11:56:14 -0400

    cambios del 03/04/2024 a las 12:00 A.M.

--- src/app/reporte/reporte.component.ts
+++ src/app/reporte/reporte.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class ReporteComponent {
 
+  asignacion: boolean = false;
+  mantenimiento: boolean= false;
+  entrada: boolean= false;
+  registros: boolean = false;
+  todo: boolean = false;
+
+  constructor() { }
+
+  toggleasignacion(event: any) {
+    this.asignacion = event.checked;
+  }
+  togglematenimiento(event: any) {
+    this.mantenimiento = event.checked;
+  }
+  toggleentrada(event: any) {
+    this.entrada = event.checked;
+  }
+  toggleregistros(event: any) {
+    this.registros = event.checked;
+}
+  toggletodo(event: any) {
+    this.todo = event.checked;
+  }
 }
