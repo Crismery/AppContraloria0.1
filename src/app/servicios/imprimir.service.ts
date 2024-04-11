d8c61c575d2ebdc61d136e77382f0dd848ee6c71
Crismery <crismerydisla0550@gmail.com>
2024-04-11 11:59:10 -0400

    cambios del 11/04/2024 a las 12:00 P.M.

--- src/app/servicios/imprimir.service.ts
+++ src/app/servicios/imprimir.service.ts
@@ -1,3 +1,4 @@
+import { Xtb } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { jsPDF } from "jspdf";
 import autoTable from 'jspdf-autotable';
@@ -19,7 +20,7 @@ export class ImprimirService {
     doc.setFontSize(10);
 
     const date = new Date();
-    const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + (date.getFullYear().toString().substr(-2));
+    const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + (date.getFullYear().toString().substr(-4));
     const timeString = date.getHours() + ':' + (date.getMinutes()<10?'0':'') + date.getMinutes();
       
     doc.text(dateString + ', ' + timeString, 10, 10);
@@ -29,21 +30,32 @@ export class ImprimirService {
     const posicionCentro1 = (doc.internal.pageSize.width - anchoAppName) / 2;
     doc.text(appName, posicionCentro1, 10);
 
-    doc.text(titulo, doc.internal.pageSize.width / 2, 25, { align: 'center' });
+
+    const img1Y = 25;
+    const img1Height = 30;
+    const img2Y = 75;
+    const img2Height = 20;
+    const tableY = 120;
+    const titleY = (img2Y + img2Height + tableY) / 2;
+
+    doc.text(titulo, doc.internal.pageSize.width / 2, titleY, { align: 'center' });
 
     const img1 = 'assets/Captura.png'; 
-    doc.addImage(img1, 'PNG', 5, 25, 30, 30); 
-    doc.text('CONTRALORIA | REPORTE', 30, 40); 
+    doc.addImage(img1, 'PNG', 25, img1Y, 30, img1Height); 
+    doc.setTextColor(200,200,200);
+    doc.text('CONTRALORIA | REPORTE', 25 + 30, img1Y + img1Height + 0); 
 
+    doc.setDrawColor(211, 211, 211);
     doc.setLineWidth(1.7); 
-    doc.line(6, 60, doc.internal.pageSize.width - 6, 60); 
+    doc.line(20, 60, doc.internal.pageSize.width - 20, 60); 
 
     const img2 = 'assets/reporte.png'; 
-    doc.addImage(img2, 'PNG', 5, 80, 25, 25); 
-    doc.text('Reporte', 30, 100); 
+    doc.addImage(img2, 'PNG', 35, img2Y, 20, img2Height); 
+    doc.setTextColor(200,200,200);
+    doc.text('Reporte', 35 + 25, img2Y + img2Height + 0); 
 
     autoTable(doc, {
-      startY: 110, 
+      startY: tableY, 
       columnStyles: { europe: { halign: 'center' } },
       head: [encabezado],
       body: cuerpo,
@@ -64,44 +76,55 @@ export class ImprimirService {
       format: 'letter'
     });
 
-    let yPosition = 25;
+    let yPositionTitulo = 100; 
+
+    doc.setFontSize(10);
 
     titulos.forEach((titulo, index) => {
       doc.setFontSize(10);
+      
 
       const date = new Date();
       const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + (date.getFullYear().toString().substr(-2));
       const timeString = date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
 
+      doc.setFont("helvetica","normal");
       doc.text(dateString + ', ' + timeString, 10, 10);
 
       const appName = "Appcontraloria";
       const anchoAppName = doc.getTextWidth(appName);
       const posicionCentro1 = (doc.internal.pageSize.width - anchoAppName) / 2;
+      doc.setFont("helvetica","normal");
       doc.text(appName, posicionCentro1, 10);
 
       const anchoTitulo = doc.getTextWidth(titulo);
       const posicionCentro = (doc.internal.pageSize.width - anchoTitulo) / 2;
 
-      doc.text(titulo, posicionCentro, yPosition);
-
-      yPosition += 25;
+      doc.setFont("helvetica","normal");
+      doc.text(titulo, posicionCentro, yPositionTitulo);
 
       const img1 = 'assets/Captura.png'; 
-    doc.addImage(img1, 'PNG', 5, 25, 30, 30); 
-    doc.text('CONTRALORIA | REPORTE', 30, 40); 
+      doc.addImage(img1, 'PNG', 25, 25, 30, 30); 
+      doc.setFont("helvetica","normal");
+      doc.setTextColor(200,200,200);
+      doc.text('CONTRALORIA | REPORTE', 25 + 30, 50); 
+  
+      doc.setDrawColor(211, 211, 211);
+      doc.setLineWidth(1.7); 
+      doc.line(20, 60, doc.internal.pageSize.width - 20, 60); 
+  
+      const img2 = 'assets/reporte.png'; 
+      doc.addImage(img2, 'PNG', 35, 75, 20, 20); 
+      doc.setFont("helvetica","normal");
+      doc.setTextColor(200,200,200);
+      doc.text('Reporte', 35 + 25, 90); 
 
-    doc.setLineWidth(1.7); 
-    doc.line(6, 60, doc.internal.pageSize.width - 6, 60); 
-
-    const img2 = 'assets/reporte.png'; 
-    doc.addImage(img2, 'PNG', 5, 80, 25, 25); 
-    doc.text('Reporte', 30, 100); 
+      const yPositionTabla = 40 + yPositionTitulo;
 
       autoTable(doc, {
         head: [encabezados[index]],
         body: cuerpos[index],
-        startY: yPosition,
+        startY: yPositionTabla,
         margin: { top: 10 }
       });
 
@@ -110,7 +133,7 @@ export class ImprimirService {
 
       const tableHeight = numberOfRows * rowHeight + (numberOfRows * rowHeight * 0.1);
 
-      yPosition += tableHeight + 40;
+      yPositionTitulo += tableHeight + 75; 
     });
 
     if (guardar) {
@@ -119,5 +142,5 @@ export class ImprimirService {
     } else {
       doc.output('dataurlnewwindow');
     }
-  }
+}
 }
